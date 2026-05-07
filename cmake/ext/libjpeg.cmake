496f0780c974106bec23e9cdcae454c3c7a9e22e
Dmitry Slepichev <dslepichev@nvidia.com>
2026-05-07 10:12:57 -0700

    [infra] Switch libjpeg to FetchContent

--- cmake/ext/libjpeg.cmake
+++ cmake/ext/libjpeg.cmake
@@ -12,42 +12,62 @@
 # By using, reproducing, modifying, distributing, performing, or displaying any portion or element
 # of the software or derivative works thereof, you agree to be bound by this License.
 
-include(ExternalProject)
+include(FetchContent)
 
-# Only build if not already built
 set(LIBJPEG_VERSION "3.1.3")
 set(LIBJPEG_INSTALL_DIR ${CMAKE_BINARY_DIR}/third_party/_install/libjpeg-turbo-${LIBJPEG_VERSION})
+set(LIBJPEG_LIBRARY ${LIBJPEG_INSTALL_DIR}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}jpeg${CMAKE_STATIC_LIBRARY_SUFFIX})
 
-# Pre-create directories so CMake doesn't complain about non-existent paths
-# (ExternalProject creates files at build-time, but CMake validates at configure-time)
-file(MAKE_DIRECTORY ${LIBJPEG_INSTALL_DIR})
+FetchContent_Declare(
+    libjpeg
+    URL https://github.com/libjpeg-turbo/libjpeg-turbo/releases/download/${LIBJPEG_VERSION}/libjpeg-turbo-${LIBJPEG_VERSION}.tar.gz
+    URL_HASH SHA256=075920b826834ac4ddf97661cc73491047855859affd671d52079c6867c1c6c0
+)
+
+FetchContent_GetProperties(libjpeg)
+if(NOT libjpeg_POPULATED)
+    FetchContent_Populate(libjpeg)
+endif()
+
+# Pre-create directories so CMake does not reject imported target include paths at configure time.
 file(MAKE_DIRECTORY ${LIBJPEG_INSTALL_DIR}/include)
 file(MAKE_DIRECTORY ${LIBJPEG_INSTALL_DIR}/lib)
 
+# libjpeg-turbo rejects add_subdirectory(), so FetchContent only fetches the source. Build it with a standalone
+# CMake invocation and expose the installed archive through an imported target.
 if(NOT TARGET libjpeg_external)
-    ExternalProject_Add(libjpeg_external
-        URL https://github.com/libjpeg-turbo/libjpeg-turbo/releases/download/${LIBJPEG_VERSION}/libjpeg-turbo-${LIBJPEG_VERSION}.tar.gz
-        URL_HASH SHA256=075920b826834ac4ddf97661cc73491047855859affd671d52079c6867c1c6c0
-        PREFIX ${CMAKE_BINARY_DIR}/third_party/libjpeg-turbo-${LIBJPEG_VERSION}
-        CMAKE_ARGS
+    add_custom_command(
+        OUTPUT ${LIBJPEG_LIBRARY}
+        COMMAND
+            ${CMAKE_COMMAND}
+            -S ${libjpeg_SOURCE_DIR}
+            -B ${libjpeg_BINARY_DIR}
             -DCMAKE_INSTALL_PREFIX=${LIBJPEG_INSTALL_DIR}
             -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
             -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}
             -DCMAKE_POSITION_INDEPENDENT_CODE=ON
             -DENABLE_SHARED=OFF
-            -DWITH_SIMD=false
-        BUILD_BYPRODUCTS
-            ${LIBJPEG_INSTALL_DIR}/lib/libjpeg.a
-            ${LIBJPEG_INSTALL_DIR}/lib/libturbojpeg.a
+            -DENABLE_STATIC=ON
+            -DWITH_SIMD=OFF
+            -DWITH_TOOLS=OFF
+            -DWITH_TESTS=OFF
+            -DWITH_TURBOJPEG=OFF
+        COMMAND
+            ${CMAKE_COMMAND}
+            --build ${libjpeg_BINARY_DIR}
+            --target install
+        COMMENT "Building libjpeg-turbo ${LIBJPEG_VERSION}"
+        VERBATIM
     )
+
+    add_custom_target(libjpeg_external DEPENDS ${LIBJPEG_LIBRARY})
 endif()
 
-# Create imported target for use in main project
-# TODO: must link different libs depending on a target system
+# Create modern namespaced target for consistency.
 if(NOT TARGET jpeg::jpeg)
     add_library(jpeg::jpeg STATIC IMPORTED GLOBAL)
     set_target_properties(jpeg::jpeg PROPERTIES
-        IMPORTED_LOCATION ${LIBJPEG_INSTALL_DIR}/lib/libjpeg.a
+        IMPORTED_LOCATION ${LIBJPEG_LIBRARY}
         INTERFACE_INCLUDE_DIRECTORIES ${LIBJPEG_INSTALL_DIR}/include
     )
     add_dependencies(jpeg::jpeg libjpeg_external)
